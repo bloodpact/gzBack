214f56d4883b3c1400032ed576fe423119f17002
unknown <toxep@mail.ru>
2019-11-11 20:46:23 +0300

    email links fix

--- routes/email.js
+++ routes/email.js
@@ -1,35 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const nodeMailer = require('nodemailer')
-const hbs = require('nodemailer-express-handlebars');
-const {hbsOpts, transportOpts} = require('../helpers/emailOpts');
+const nodeMailer = require('nodemailer');
+const {transportOpts} = require('../helpers/emailOpts');
+router.get('/', function (req, res) {
+    async function main() {
+        let linksArr = req.query.links.map(el=>{
+            if(!el.includes("http")){
+                el = `<a href="http://zakupki.gov.ru${el}">http://zakupki.gov.ru${el}</a><br>`
+            } else{
+                el = `<a href="${el}">${el}</a><br>`
+            }
+            return el
+        });
 
+        let transporter = nodeMailer.createTransport(transportOpts);
 
-router.get('/', function (req, res, next) {
-    let transporter = nodeMailer.createTransport(transportOpts);
-    transporter.use('compile', hbs(hbsOpts));
-    let linksArr = req.query.links.map(el=>{
-        if(!el.includes("http")){
-            el = 'http://zakupki.gov.ru' + el
-        }
-        return el
-    });
-
-    let mailOptions = {
-        from:'zakupkigov.grabber@mail.ru',
-        subject: 'tenders',
-        to: req.query.user,
-        context:{
-            links: linksArr
-        }
-    };
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message %s sent: %s', info.messageId, info.response);
-    });
-    res.end();
+        let info = await transporter.sendMail({
+            from: 'zakupkigov.grabber@mail.ru', // sender address
+            to: req.query.user, // list of receivers
+            subject: 'Тендеры', // Subject line
+            text: 'Тендеры', // plain text body
+            html: `<p>${linksArr}</p>` // html body
+        });
+        console.log('Message sent: %s', info.messageId);
+    }
+    main().then(res.end()).catch(console.error);
 });
-
 module.exports = router;
